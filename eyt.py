33cf7a3b97b64f57e4aeb44bdd9fde76bb3afeba
evdokimovam-2 <slavalox2007@gmail.com>
2024-03-05 12:05:17 +0300

    gg

--- eyt.py
+++ eyt.py
@@ -1,7 +1,31 @@
-A = [2, 4, 1, 7, 3, 5, 11]
-s = 1
-for i in range(3):
-    if A[i] < A[i+1]:
-        A[i+1], A[i] = A[i], A[i+1]
-        s += A[i]
-print(s)
+class Studentiki:
+    pass
+
+
+students = []
+f = open('students(2).csv')
+name = f.readline().strip()
+j = 0
+for i in f:
+    s = i.strip().split(',')
+    students.append(Studentiki())
+    students[j].userid = s[0]
+    students[j].fio = s[1]
+    students[j].title = s[2]
+    students[j].clas = s[3]
+    students[j].score = s[4]
+    j += 1
+for i in students:
+    s = 0
+    k = 0
+    if i.score == 'None':
+        for j in students:
+            if j.clas == i.clas and j.score != 'None':
+                k += 1
+                s += int(j.score)
+        i.score = f'{s / k:.3f}'
+f = open('students_new.csv', 'w')
+print(name, file=f)
+for i in students:
+    print(i.userid, ',', i.fio, ',', i.title, ',', i.clas, ',', i.score, file=f)
+f.close()
