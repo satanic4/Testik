d9d6219a39c0a8c05119cd24555e4b17e4343631
evdokimovam-2 <slavalox2007@gmail.com>
2024-03-05 12:41:04 +0300

    3 nomer

--- nomer 2.py
+++ nomer 2.py
@@ -1,1 +1,24 @@
-print('c')
+class Studentki:
+    pass
+
+
+f = open('students(2).csv', encoding='utf-8')
+a = f.readlines()
+students = []
+for i in a[1:]:
+    inf = Studentki()
+    inn = i.split('.')
+    inf_id = inn[0]
+    inf.fio = inn[1]
+    inf.prid = int(inn[2])
+    inf.clas = inn[3]
+    inf.score = inn[4]
+    students.append(inf)
+
+index_chela = int(input())
+if index_chela > 500:
+    print('ты шо, куку?')
+else:
+    for j in students:
+        if j.prid == index_chela:
+            print('Проект № ', j.prid, 'делал ', j.fio, 'он получил оценку', j.score)
