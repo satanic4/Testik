38721c50dccdf3b255789e313d3e2af7feb46434
Артем <slavalox2007@gmail.com>
2024-03-05 20:41:31 +0300

    1 number

---
+++ number 1.py
@@ -0,0 +1,34 @@
+class Studentiki:
+    pass
+
+
+students = []
+f = open('students(2).csv')
+name = f.readline().strip()
+j = 0
+
+
+
+for i in f:
+    s = i.strip().split(',')
+    students.append(Studentiki())
+    students[j].userid = s[0]
+    students[j].fio = s[1]
+    students[j].title = s[2]
+    students[j].clas = s[3]
+    students[j].score = s[4]
+    j += 1
+for i in students:
+    s = 0
+    k = 0
+    if i.score == 'None':
+        for j in students:
+            if j.clas == i.clas and j.score != 'None':
+                k += 1
+                s += int(j.score)
+        i.score = f'{s / k:.3f}'
+f = open('students_new.csv', 'w')
+print(name, file=f)
+for i in students:
+    print(i.userid, ',', i.fio, ',', i.title, ',', i.clas, ',', i.score, file=f)
+f.close()
