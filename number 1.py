d972773600ac4ac4ecf5d62af5a1502eb6950450
Артем <slavalox2007@gmail.com>
2024-03-05 21:07:57 +0300

    1 number

--- number 1.py
+++ number 1.py
@@ -1,21 +1,34 @@
-class Studentki:
+class Studentiki:
     pass
 
 
-f = open('students(2).csv')
-a = f.readlines()
 students = []
-for i in a[1:]:
-    inf = Studentki()
-    inn = i.split('.')
-    inf_id = inn[0]
-    inf.fio = inn[1]
-    inf.prid = int(inn[2])
-    inf.clas = inn[3]
-    inf.score = inn[4]
-    students.append(inf)
+f = open('students(2).csv')
+name = f.readline().strip()
+j = 0
 
-index_chela = int(input())
-for j in students:
-    if j.prid == index_chela:
-    print('Проект № ', j.prid, 'делал ', j.fio, 'он получил оценку', j.score)
+
+
+for i in f:
+    s = i.strip().split(',')
+    students.append(Studentiki())
+    students[j].userid = s[0]
+    students[j].fio = s[1]
+    students[j].title = s[2]
+    students[j].clas = s[3]
+    students[j].score = s[4]
+    j += 1
+for i in students:
+    s = 0
+    k = 0
+    if i.score == 'None':
+        for j in students:
+            if j.clas == i.clas and j.score != 'None':
+                k += 1
+                s += int(j.score)
+        i.score = f'{s / k:.3f}'
+f = open('students_new.csv', 'w')
+print(name, file=f)
+for i in students:
+    print(i.userid, ',', i.fio, ',', i.title, ',', i.clas, ',', i.score, file=f)
+f.close()
